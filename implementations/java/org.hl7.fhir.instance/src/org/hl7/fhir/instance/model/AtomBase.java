bdcaac7bc460238181b519ec8d5443af84cc1f3a
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2013-09-06 07:54:03 +0000

    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@1731 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/AtomBase.java
@@ -0,0 +1,58 @@
+package org.hl7.fhir.instance.model;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class AtomBase {
+
+  private String id;
+  private Map<String, String> links = new LinkedHashMap<String, String>();
+  private Map<String, String> tags = new HashMap<String, String>();
+  private java.util.Calendar updated;
+  private String authorName;
+  private String authorUri;
+  private String title;
+
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public Map<String, String> getLinks() {
+    return links;
+  }
+  
+  public String getAuthorName() {
+    return authorName;
+  }
+  public void setAuthorName(String authorName) {
+    this.authorName = authorName;
+  }
+  public String getAuthorUri() {
+    return authorUri;
+  }
+  public void setAuthorUri(String authorUri) {
+    this.authorUri = authorUri;
+  }
+  public java.util.Calendar getUpdated() {
+    return updated;
+  }
+  public void setUpdated(java.util.Calendar updated) {
+    this.updated = updated;
+  }
+	public Map<String, String> getTags() {
+		return tags;
+	}
+
+  
+}
