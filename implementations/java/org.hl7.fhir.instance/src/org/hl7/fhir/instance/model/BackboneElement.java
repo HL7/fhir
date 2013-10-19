f0f52894216dc8b95523084dd171a3a7363c1e0e
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2013-10-19 10:18:15 +0000

    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@1868 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/BackboneElement.java
@@ -0,0 +1,64 @@
+package org.hl7.fhir.instance.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BackboneElement extends Element {
+
+  private List<Extension> modifierExtensions = new ArrayList<Extension>();
+   
+  
+  
+  public List<Extension> getModifierExtensions() {
+		return modifierExtensions;
+	}
+
+	public boolean hasExtensions() {
+    return modifierExtensions.size() > 0 || super.hasExtensions();
+  }
+	
+  public boolean hasExtension(String name) {
+    if (name == null)
+      return false;
+    for (Extension e : modifierExtensions) {
+      if (name.equals(e.getUrlSimple()))
+        return true;
+    }
+    return super.hasExtension(name);
+  }
+
+  public Extension getExtension(String name) {
+    if (name == null)
+      return null;
+    for (Extension e : modifierExtensions) {
+      if (name.equals(e.getUrlSimple()))
+        return e;
+    }
+    return super.getExtension(name);
+  }
+  
+  public boolean hasModifierExtensions() {
+    return modifierExtensions.size() > 0;
+  }
+	
+  public boolean hasModifierExtension(String name) {
+    if (name == null)
+      return false;
+    for (Extension e : modifierExtensions) {
+      if (name.equals(e.getUrlSimple()))
+        return true;
+    }
+    return false;
+  }
+
+  public Extension getModifierExtension(String name) {
+    if (name == null)
+      return null;
+    for (Extension e : modifierExtensions) {
+      if (name.equals(e.getUrlSimple()))
+        return e;
+    }
+    return null;
+  }
+  
+}
