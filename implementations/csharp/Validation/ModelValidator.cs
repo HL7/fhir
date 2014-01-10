33d038f9411ee07817de879b4aa3f450eef03ef3
ewoutkramer <ewoutkramer@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-01-10 09:57:17 +0000

    - Jar now includes generation of net45 project template
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@2133 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/csharp/Validation/ModelValidator.cs
@@ -0,0 +1,32 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Hl7.Fhir.Validation
+{
+    public class ModelValidator
+    {
+        public static bool TryValidate(Resource resource, ICollection<ValidationResult> validationResults)
+        {
+            return Validator.TryValidateObject(resource, ValidationContextFactory.Create(resource, null), validationResults, true);
+        }
+
+        public static bool TryValidate(Element element, ICollection<ValidationResult> validationResults)
+        {
+            return Validator.TryValidateObject(element, ValidationContextFactory.Create(element, null), validationResults, true);
+        }
+
+        public static void Validate(Resource resource)
+        {
+            Validator.ValidateObject(resource, ValidationContextFactory.Create(resource, null), true);
+        }
+
+        public static void Validate(Element element)
+        {
+            Validator.ValidateObject(element, ValidationContextFactory.Create(element, null), true);
+        }
+    }
+}
