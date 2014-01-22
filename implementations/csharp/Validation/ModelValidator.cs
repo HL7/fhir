ed435a447194c1ade69caf8122369969bc52fdfc
ewoutkramer <ewoutkramer@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-01-22 16:27:06 +0000

    - Additional operations for validation
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@2190 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/csharp/Validation/ModelValidator.cs
+++ implementations/csharp/Validation/ModelValidator.cs
@@ -9,44 +9,60 @@ namespace Hl7.Fhir.Validation
 {
     public class ModelValidator
     {
-        public static bool TryValidate(Resource resource, ICollection<ValidationResult> validationResults)
-        {
-            return Validator.TryValidateObject(resource, ValidationContextFactory.Create(resource, null), validationResults, true);
-        }
-
-        public static bool TryValidate(Element element, ICollection<ValidationResult> validationResults)
-        {
-            return Validator.TryValidateObject(element, ValidationContextFactory.Create(element, null), validationResults, true);
-        }
-
-        public static bool TryValidate(ResourceEntry entry, ICollection<ValidationResult> validationResults)
-        {
-            return Validator.TryValidateObject(entry, ValidationContextFactory.Create(entry, null), validationResults, true);
-        }
-
-        public static bool TryValidate(Bundle bundle, ICollection<ValidationResult> validationResults)
-        {
-            return Validator.TryValidateObject(bundle, ValidationContextFactory.Create(bundle, null), validationResults, true);
-        }
-
-        public static void Validate(ResourceEntry entry)
-        {
-            Validator.ValidateObject(entry, ValidationContextFactory.Create(entry, null), true);
-        }
-
         public static void Validate(Resource resource)
         {
+            if (resource == null) throw new ArgumentNullException("resource");
             Validator.ValidateObject(resource, ValidationContextFactory.Create(resource, null), true);
+        }
+
+        public static bool TryValidate(Resource resource, ICollection<ValidationResult> validationResults=null)
+        {
+            if(resource == null) throw new ArgumentNullException("resource");
+
+            var results = validationResults ?? new List<ValidationResult>();
+            return Validator.TryValidateObject(resource, ValidationContextFactory.Create(resource, null), results, true);
         }
 
         public static void Validate(Element element)
         {
+            if (element == null) throw new ArgumentNullException("element");
             Validator.ValidateObject(element, ValidationContextFactory.Create(element, null), true);
+        }
+
+        public static bool TryValidate(Element element, ICollection<ValidationResult> validationResults = null)
+        {
+            if (element == null) throw new ArgumentNullException("element");
+
+            var results = validationResults ?? new List<ValidationResult>();
+            return Validator.TryValidateObject(element, ValidationContextFactory.Create(element, null), results, true);
+        }
+
+        public static void Validate(ResourceEntry entry)
+        {
+            if (entry == null) throw new ArgumentNullException("entry");
+            Validator.ValidateObject(entry, ValidationContextFactory.Create(entry, null), true);
+        }
+
+        public static bool TryValidate(ResourceEntry entry, ICollection<ValidationResult> validationResults = null)
+        {
+            if (entry == null) throw new ArgumentNullException("entry");
+
+            var results = validationResults ?? new List<ValidationResult>();
+            return Validator.TryValidateObject(entry, ValidationContextFactory.Create(entry, null), results, true);
         }
 
         public static void Validate(Bundle bundle)
         {
+            if (bundle == null) throw new ArgumentNullException("bundle");
             Validator.ValidateObject(bundle, ValidationContextFactory.Create(bundle, null), true);
+        }
+
+        public static bool TryValidate(Bundle bundle, ICollection<ValidationResult> validationResults = null)
+        {
+            if (bundle == null) throw new ArgumentNullException("bundle");
+
+            var results = validationResults ?? new List<ValidationResult>();
+            return Validator.TryValidateObject(bundle, ValidationContextFactory.Create(bundle, null), results, true);
         }
     }
 
