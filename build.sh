c207ce0a442cb2158f7875950a97f8aab3bb8c70
josh_mandel <josh_mandel@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-04-09 23:04:54 +0000

    Add build script to support continuous integration
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@2334 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ build.sh
@@ -0,0 +1,31 @@
+NAME=travis_build
+
+prepareToBuild (){
+  git clean -d -f
+}
+
+toolsBuild (){
+  prepareToBuild
+  java -jar -Xmx1024m tools/bin/org.hl7.fhir.tools.jar `pwd` -name "$NAME"
+  checkStatus
+}
+
+antBuild (){
+  prepareToBuild
+  ant -f tools/java/org.hl7.fhir.tools.core/build.xml cleanall Publisher -Dargs=\"$(pwd) -name $NAME\"
+  checkStatus
+}
+
+checkStatus (){
+  if [ $? -eq 0 -a ! -f fhir-error-dump.txt ]
+  then
+    echo "Build sttus OK"
+  else
+    echo "error dump:"
+    cat fhir-error-dump.txt
+    exit 1
+  fi
+}
+
+antBuild
+toolsBuild
