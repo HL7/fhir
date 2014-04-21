58210d05866562d4636de68dba5e438dc7b63033
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-04-21 22:50:05 +0000

    Change the way the tools copy of the reference implementation is synchronised
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@2386 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/copy current generated code.bat
+++ tools/java/copy current generated code.bat
@@ -1,9 +1,10 @@
-del C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\formats\*.java
-del C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\model\*.java
-copy C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\formats\*.java C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\formats
-copy C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\model\*.java C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\model
-copy C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\utils\*.java C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\utils
-copy C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\validation\*.java C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\validation
-copy C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\test\*.java C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\test
-copy C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\client\*.java C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\client
+@echo off
+REM Dirmerge is a utility that scans source, and copies to dest. if the file already exists in source, it checks if they are different, and if they are, brings up winmerge if the genmark is not found
+C:\work\org.hl7.fhir\build\tools\bin\dirmerge C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\formats C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\formats java "// Generated on"
+C:\work\org.hl7.fhir\build\tools\bin\dirmerge C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\model C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\model java "// Generated on"
+C:\work\org.hl7.fhir\build\tools\bin\dirmerge C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\utils C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\utils java
+C:\work\org.hl7.fhir\build\tools\bin\dirmerge C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\validation C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\validation java
+C:\work\org.hl7.fhir\build\tools\bin\dirmerge C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\test C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\test java
+C:\work\org.hl7.fhir\build\tools\bin\dirmerge C:\work\org.hl7.fhir\build\implementations\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\client C:\work\org.hl7.fhir\build\tools\java\org.hl7.fhir.instance\src\org\hl7\fhir\instance\client java
+
 
