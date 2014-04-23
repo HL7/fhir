cff852aca348668f2bcecbd588fba194c6efec9a
ewoutkramer <ewoutkramer@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-04-23 21:33:03 +0000

    Fixed determination of JAVA_HOME in publish.bat for use with ANT
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@2406 2f0db536-2c49-4257-a3fa-e771ed206c19

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 @ECHO off
 ECHO Running publication process now
 
-for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newestJDK=%ProgramFiles%\java\%%x\bin\java.exe
-set JAVA_HOME=%newestJDK% 
+for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newestJDK=%ProgramFiles%\java\%%x
+set JAVA_HOME=%newestJDK%
 echo Using %JAVA_HOME%
 
 ant Publisher -Dargs="%*"
