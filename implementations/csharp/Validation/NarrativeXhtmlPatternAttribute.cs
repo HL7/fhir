23dfe16e69835a014797c3096e5400d3472b86a7
ewoutkramer <ewoutkramer@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-04-24 09:05:28 +0000

    - [C#] Better error reports on validation errors - [C#] Updates for PCL 4.5
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@2413 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/csharp/Validation/NarrativeXhtmlPatternAttribute.cs
+++ implementations/csharp/Validation/NarrativeXhtmlPatternAttribute.cs
@@ -15,18 +15,20 @@ namespace Hl7.Fhir.Validation
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     public class NarrativeXhtmlPatternAttribute : ValidationAttribute
     {
+#if !PORTABLE45
         private static Lazy<XmlSchemaSet> schemaSet = new Lazy<XmlSchemaSet>(compileXhtmlSchema, true);
 
         private static XmlSchemaSet compileXhtmlSchema()
         {
-            Stream s = typeof(NarrativeXhtmlPatternAttribute).Assembly.GetManifestResourceStream("Hl7.Fhir.Model.Support.fhir-xhtml1-strict.xsd");
+            Stream s = typeof(NarrativeXhtmlPatternAttribute).Assembly.GetManifestResourceStream("Hl7.Fhir.Model.fhir-xhtml1-strict.xsd");
             XmlSchemaSet schemas = new XmlSchemaSet();
             schemas.Add("http://www.w3.org/1999/xhtml", XmlReader.Create(s));
 
             return schemas;
         }
+#endif
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
 
             if (value == null) return ValidationResult.Success;
@@ -35,16 +37,21 @@ namespace Hl7.Fhir.Validation
                 throw new ArgumentException("CodePatternAttribute can only be applied to string properties");
 
             try
-            {
+			{
+				// There is currently no validation in the portable .net
+				// for the XDocument validation, would need to scan for
+				// another implementation to cover this
+#if !PORTABLE45
                 var doc = XDocument.Parse(value as string);
                 doc.Validate(schemaSet.Value, null, false);
+#endif
 
-                return ValidationResult.Success;
+				return ValidationResult.Success;
             }
             catch(Exception e)
             {
                 return FhirValidator.BuildResult(validationContext, "Xml can not be parsed or is not valid: " + e.Message);
             }
         }
-    }
+	}
 }
