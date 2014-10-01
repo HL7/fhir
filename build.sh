e266595babe94cea5db4e183f1a070a786a873cf
josh_mandel <josh_mandel@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-10-01 01:27:02 +0000

    Avoid advanced grep features for compatibility with Travis CI
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@2952 2f0db536-2c49-4257-a3fa-e771ed206c19

--- build.sh
+++ build.sh
@@ -2,19 +2,7 @@
 set -ev
 
 NAME="Continuous Integration Build"
-SVNREV='svn-rev'
-
-echo "Current dir"
-pwd
-
-echo "Git log"
-git log -1 
-
-echo "Git log svn/fhir line"
-git log -1 | grep svn.fhir. 
-
-echo "Git log svn rev"
-git log -1 | grep svn.fhir. | grep -oP 'svn.fhir.\K([0-9]+)'
+SVNREV=$(git log -1 | grep svn.fhir. | sed -r 's/^.*?@([0-9]+).*$/\1/')
 
 antBuild (){
   ./publish.sh -svn $SVNREV -name \'$NAME\' -url http://hl7-fhir.github.io/
