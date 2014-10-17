6ef04d73bff2d253cd4a288371f40582b709df0d
josh_mandel <josh_mandel@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-10-17 01:45:37 +0000

    Defensively check for low file count before publishing
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3104 2f0db536-2c49-4257-a3fa-e771ed206c19

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ antBuild (){
 }
 
 checkStatus (){
+  nf=`find publish -maxdepth 1 -type f | wc -l`
+  if [ "$nf" -lt "100" ] ; then
+     echo "< 100 files produced: bailing!"
+     exit 1
+  fi
   if [ $? -eq 0 -a ! -f fhir-error-dump.txt ]
   then
     echo "Build status OK"
