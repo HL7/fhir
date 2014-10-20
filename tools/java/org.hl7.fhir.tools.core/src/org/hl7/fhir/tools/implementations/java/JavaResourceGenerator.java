16a4c17599cc73d4b5356841dc2cf94810b86719
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-10-20 05:09:51 +0000

    debugging code
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3196 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/implementations/java/JavaResourceGenerator.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/implementations/java/JavaResourceGenerator.java
@@ -691,7 +691,7 @@ public class JavaResourceGenerator extends JavaBaseGenerator {
         jdoc(indent, "@param value {@link #"+getElementName(e.getName(), true)+"} ("+e.getDefinition()+")");
         write(indent+"public boolean has"+getTitle(getElementName(e.getName(), false))+"("+getSimpleType(tn)+" value) { \r\n");
         write(indent+"  for ("+tn+" v : this."+getElementName(e.getName(), true)+")\r\n");
-        write(indent+"    if (v.getValue().equals(value))\r\n");
+        write(indent+"    if (v.getValue().equals(value)) // "+e.typeCode()+"\r\n");
         write(indent+"      return true;\r\n");
         write(indent+"  return false;\r\n");
         write(indent+"}\r\n");
