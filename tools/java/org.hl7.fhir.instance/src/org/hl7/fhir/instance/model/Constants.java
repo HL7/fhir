ae57a7abccd3a646bddc61e2fae3f56146df32c8
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-10-20 00:07:43 +0000

    fix narrative generation error
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3189 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Constants.java
+++ tools/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Constants.java
@@ -29,12 +29,12 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Mon, Oct 20, 2014 10:47+1100 for FHIR v0.3.0
+// Generated on Mon, Oct 20, 2014 11:01+1100 for FHIR v0.3.0
 
 
 public class Constants {
 
   public final static String VERSION = "0.3.0";
-  public final static String REVISION = "3183";
-  public final static String DATE = "Mon Oct 20 10:47:24 EST 2014";
+  public final static String REVISION = "3188";
+  public final static String DATE = "Mon Oct 20 11:01:25 EST 2014";
 }
--- tools/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/ResourceFactory.java
+++ tools/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/ResourceFactory.java
@@ -29,7 +29,7 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Mon, Oct 20, 2014 10:47+1100 for FHIR v0.3.0
+// Generated on Mon, Oct 20, 2014 11:01+1100 for FHIR v0.3.0
 
 public class ResourceFactory extends Factory {
 
