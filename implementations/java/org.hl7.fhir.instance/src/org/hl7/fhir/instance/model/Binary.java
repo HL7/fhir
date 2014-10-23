0442a1884bc18cb30981217c360bac3a12cea144
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-10-23 01:37:34 +0000

    {#3522} minor fixes to extension definition
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3244 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Binary.java
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Binary.java
@@ -29,7 +29,7 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Thu, Oct 23, 2014 12:13+1100 for FHIR v0.3.0
+// Generated on Thu, Oct 23, 2014 12:31+1100 for FHIR v0.3.0
 
 import java.util.*;
 
