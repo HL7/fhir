39fb3f866be9936d3c1b27f3f925ff58061d18e0
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-11-12 12:55:48 +0000

    documentation of new resource structure
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3366 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
@@ -29,7 +29,7 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Wed, Nov 12, 2014 22:24+1100 for FHIR v0.3.0
+// Generated on Wed, Nov 12, 2014 23:50+1100 for FHIR v0.3.0
 
 import java.util.*;
 
