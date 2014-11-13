90360ca5ada92d4cf480cc1b8bfbbecd62fd574b
markkramerus <markkramerus@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-11-13 16:19:22 +0000

    Un-breaking the build, hopefullly.  Fixed the medication-cqf-profile-spreadsheet.xml.
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3389 2f0db536-2c49-4257-a3fa-e771ed206c19

--- publish.bat
+++ publish.bat
@@ -5,6 +5,7 @@ for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newest
 set JAVA_HOME=%newestJDK%
 echo Using %JAVA_HOME%
 
+set ANT_OPTS=-Dhttp.proxyPort=80 -Dhttp.proxyHost=gatekeeper.mitre.org
 call ant Publisher -Dargs="%*"
 
 PAUSE
