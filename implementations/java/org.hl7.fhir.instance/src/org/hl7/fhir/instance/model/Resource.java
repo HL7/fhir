c9a4d729af24d190d239dde9f4cfa98904e81830
paulknapp <paulknapp@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-11-22 19:29:58 +0000

    Add StatusResponse
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3513 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
@@ -29,7 +29,7 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Fri, Nov 21, 2014 20:09-0800 for FHIR v0.3.0
+// Generated on Sat, Nov 22, 2014 11:15-0800 for FHIR v0.3.0
 
 import java.util.*;
 
