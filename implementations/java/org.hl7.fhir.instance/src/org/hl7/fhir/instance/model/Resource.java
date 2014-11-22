540609026df98f785cfca1b8af1609b8ddf36695
paulknapp <paulknapp@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-11-22 20:45:56 +0000

    Added Enrollment resource
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3514 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
@@ -29,7 +29,7 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Sat, Nov 22, 2014 11:15-0800 for FHIR v0.3.0
+// Generated on Sat, Nov 22, 2014 12:36-0800 for FHIR v0.3.0
 
 import java.util.*;
 
