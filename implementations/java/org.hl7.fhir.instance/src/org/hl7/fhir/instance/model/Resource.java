668c74ab8f5442e5689230cb92a58b8053943b0c
ahenket <ahenket@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-11-26 21:36:05 +0000

    Added th and br to the list of allowable elements to mitigate errors deriving from extensiondefinition-introduction.xml and because they seem harmless enough
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3568 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
@@ -29,7 +29,7 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Mon, Nov 24, 2014 18:38-0800 for FHIR v0.3.0
+// Generated on Wed, Nov 26, 2014 22:29+0100 for FHIR v0.3.0
 
 import java.util.*;
 
