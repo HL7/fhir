363d7c778d74b38932037050ef84182b923ec6de
paulknapp <paulknapp@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-12-04 04:05:45 +0000

    Complete replacement of Enrollment with EnrollmentRequest
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@3690 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Resource.java
@@ -29,7 +29,7 @@ package org.hl7.fhir.instance.model;
   
 */
 
-// Generated on Wed, Dec 3, 2014 11:13-0800 for FHIR v0.3.0
+// Generated on Wed, Dec 3, 2014 19:55-0800 for FHIR v0.3.0
 
 import java.util.*;
 
