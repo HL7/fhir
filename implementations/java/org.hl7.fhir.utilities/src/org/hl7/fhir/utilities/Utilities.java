c2513e9fa12ec704257a5c9abe484447a83d24f2
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2015-06-09 23:48:23 +0000

    fix build
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@5444 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.utilities/src/org/hl7/fhir/utilities/Utilities.java
+++ implementations/java/org.hl7.fhir.utilities/src/org/hl7/fhir/utilities/Utilities.java
@@ -753,29 +753,33 @@ public class Utilities {
   // http://stackoverflow.com/questions/3780406/how-to-play-a-sound-alert-in-a-java-application
   public static float SAMPLE_RATE = 8000f;
   
-  public static void tone(int hz, int msecs) throws LineUnavailableException {
+  public static void tone(int hz, int msecs) {
       tone(hz, msecs, 1.0);
    }
 
-   public static void tone(int hz, int msecs, double vol) throws LineUnavailableException  {
-     byte[] buf = new byte[1];
-     AudioFormat af = 
-         new AudioFormat(
-             SAMPLE_RATE, // sampleRate
-             8,           // sampleSizeInBits
-             1,           // channels
-             true,        // signed
-             false);      // bigEndian
-     SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
-     sdl.open(af);
-     sdl.start();
-     for (int i=0; i < msecs*8; i++) {
-       double angle = i / (SAMPLE_RATE / hz) * 2.0 * Math.PI;
-       buf[0] = (byte)(Math.sin(angle) * 127.0 * vol);
-       sdl.write(buf,0,1);
-     }
-     sdl.drain();
-     sdl.stop();
-     sdl.close();
-   }
+  public static void tone(int hz, int msecs, double vol) {
+    try {
+      byte[] buf = new byte[1];
+      AudioFormat af = 
+          new AudioFormat(
+              SAMPLE_RATE, // sampleRate
+              8,           // sampleSizeInBits
+              1,           // channels
+              true,        // signed
+              false);      // bigEndian
+      SourceDataLine sdl;
+      sdl = AudioSystem.getSourceDataLine(af);
+      sdl.open(af);
+      sdl.start();
+      for (int i=0; i < msecs*8; i++) {
+        double angle = i / (SAMPLE_RATE / hz) * 2.0 * Math.PI;
+        buf[0] = (byte)(Math.sin(angle) * 127.0 * vol);
+        sdl.write(buf,0,1);
+      }
+      sdl.drain();
+      sdl.stop();
+      sdl.close();
+    } catch (LineUnavailableException e) {
+    }
+  }
 }
