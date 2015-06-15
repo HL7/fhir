ec87a07cac19d2577a2148fbcc47941ac3f505dc
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2015-06-15 06:07:24 +0000

    fix for java 1.6
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@5477 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.utilities/src/org/hl7/fhir/utilities/Utilities.java
+++ implementations/java/org.hl7.fhir.utilities/src/org/hl7/fhir/utilities/Utilities.java
@@ -715,7 +715,7 @@ public class Utilities {
     StringBuilder b = new StringBuilder();
     boolean lastBreak = true;
     for (char c : code.toCharArray()) {
-      if (Character.isAlphabetic(c)) {
+      if (Character.isLetter(c)) {
         if (lastBreak)
           b.append(Character.toUpperCase(c));
         else { 
