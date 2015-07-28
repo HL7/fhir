696c4075bab4f75ee97c531e4c5bd9d8f7bd2df9
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2015-07-28 03:11:10 +0000

    #8450 - explain usage of OperationOutcome.issue.location
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@6029 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
@@ -4802,7 +4802,7 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider  {
     cnt = processPageIncludes(filename, cnt, "notes", others, null, tabs, "--", ig).trim()+"\r\n";
     if (cnt.startsWith("<div")) {
       if (!cnt.startsWith(HTML_PREFIX1) && !cnt.startsWith(HTML_PREFIX2))
-        throw new Exception("unable to process start xhtml content "+filename+" : "+cnt.substring(0, HTML_PREFIX1.length())+" - should be '"+HTML_PREFIX1+"' or '"+HTML_PREFIX2+"'");
+        throw new Exception("unable to process start xhtml content "+filename+" : \r\n"+cnt.substring(0, HTML_PREFIX1.length())+" - should be \r\n'"+HTML_PREFIX1+"' or \r\n'"+HTML_PREFIX2+"'");
       else if (!cnt.endsWith(HTML_SUFFIX))
         throw new Exception("unable to process end xhtml content "+filename+" : "+cnt.substring(cnt.length()-HTML_SUFFIX.length()));
       else if (cnt.startsWith(HTML_PREFIX2))
