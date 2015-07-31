42d6bcd1e0f35bb81ca063f979a852748e5db198
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2015-07-31 05:58:56 +0000

    #8416 - remove batch operations in favour of just using batch
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@6067 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/utils/BatchLoader.java
@@ -0,0 +1,79 @@
+package org.hl7.fhir.instance.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.hl7.fhir.instance.client.FHIRSimpleClient;
+import org.hl7.fhir.instance.client.IFHIRClient;
+import org.hl7.fhir.instance.formats.IParser;
+import org.hl7.fhir.instance.formats.JsonParser;
+import org.hl7.fhir.instance.formats.XmlParser;
+import org.hl7.fhir.instance.model.Bundle;
+import org.hl7.fhir.instance.model.Bundle.BundleType;
+import org.hl7.fhir.instance.model.Resource;
+
+import com.google.gson.JsonParseException;
+
+public class BatchLoader {
+
+	public static void main(String[] args) throws Exception {
+	  if (args.length < 4) {
+	  	System.out.println("Batch uploader takes 4 parameters in order: server base url, file to upload, xml/json, and batch size");
+	  } else {
+	  	String server = args[0];
+	  	String file = args[1];
+	  	IParser p = args[2].equals("json") ? new JsonParser() : new XmlParser();
+	  	int size = Integer.parseInt(args[3]);
+	  	if (file.endsWith(".xml")) {
+	  		throw new Exception("Unimplemented file type "+file);
+	  	} else if (file.endsWith(".json")) {
+	  		throw new Exception("Unimplemented file type "+file);
+	  	} else if (file.endsWith(".zip")) {
+	  		LoadZipFile(server, file, p, size);
+	  	} else 
+	  		throw new Exception("Unknown file type "+file);
+	  }
+	}
+
+	private static void LoadZipFile(String server, String file, IParser p, int size) throws Exception {
+		System.out.println("Load Zip file "+file);
+	 	Bundle b = new Bundle();
+	 	b.setType(BundleType.COLLECTION);
+	 	b.setId(UUID.randomUUID().toString().toLowerCase());
+	 	ZipInputStream zip = new ZipInputStream(new FileInputStream(file));
+	 	ZipEntry entry;
+    while((entry = zip.getNextEntry())!=null)
+    {
+    	Resource r = p.parse(zip);
+    	b.addEntry().setResource(r);
+    }
+	 	loadBundle(server, b, size);
+	}
+
+	private static void loadBundle(String server, Bundle b, int size) throws URISyntaxException {
+		System.out.println("Post to "+server+". size = "+Integer.toString(size));
+		IFHIRClient client = new FHIRSimpleClient();
+		client.initialize(server);
+	  int c = 0;
+	  while (c < b.getEntry().size()) {
+		 	Bundle bt = new Bundle();
+		 	bt.setType(BundleType.TRANSACTION);
+		 	bt.setId(UUID.randomUUID().toString().toLowerCase());
+		 	for (int i = c; i < Math.min(b.getEntry().size(), c+size); i++) {
+		 		bt.addEntry(b.getEntry().get(i));
+		 	}
+			System.out.print(".");
+		 	client.transaction(bt);
+		 	c = c + size;
+	  	
+	  }
+		System.out.println(" done");
+	
+	}
+
+}
