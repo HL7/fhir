da4aa08fd635b27f206bd60d0fe79d7a1abcca7e
jamesagnew <jamesagnew@2f0db536-2c49-4257-a3fa-e771ed206c19>
2015-08-25 16:44:12 +0000

    Oops, missed a class!
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@6570 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/ex/NotFoundException.java
@@ -0,0 +1,10 @@
+package org.hl7.fhir.instance.ex;
+
+/**
+ * Exception indicating that a given object was not found
+ */
+public class NotFoundException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+}
