f58b32b1096a0c53b6d36098e3535c876fad011a
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2015-09-14 05:10:10 +0000

    more spelling and grammar
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@6972 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/ValueSetImporterV2.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/ValueSetImporterV2.java
@@ -752,7 +752,7 @@ public class ValueSetImporterV2 {
         String src = TextFile.fileToString(page.getFolders().srcDir + "v2" + File.separator + "template-tbl.html");
         ValueSet vs = page.getValueSets().get("http://hl7.org/fhir/ValueSet/v2-"+id);
         String sf = page.processPageIncludes(id + ".html", src, "v2Vocab", null, "v2" + File.separator + id + File.separator + "index.html", vs, null, "V2 Table", null);
-        sf = sects.addSectionNumbers("v2" + File.separator + id + ".html", "template-v2", sf, iid, 2, null, null);
+        sf = sects.addSectionNumbers("v2" + File.separator + id + File.separator +  "index.html", "template-v2", sf, iid, 2, null, null);
         TextFile.stringToFile(sf, page.getFolders().dstDir + "v2" + File.separator + id + File.separator + "index.html");
         page.getEpub().registerExternal("v2" + File.separator + id + File.separator + "index.html");        
       }
