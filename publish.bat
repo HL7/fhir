e4af0d686a49bf5cab0c00d6abfd03c62b51e4c2
brynrhodes <brynrhodes@2f0db536-2c49-4257-a3fa-e771ed206c19>
2016-02-09 03:18:44 +0000

    Updates to the CTS2 guide based on the latest Xsd-to-Fhir generator.
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@7669 2f0db536-2c49-4257-a3fa-e771ed206c19

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 @ECHO off
 ECHO Running publication process now
 
-for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newestJDK=%ProgramFiles%\java\%%x
-set JAVA_HOME=%newestJDK%
+REM for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newestJDK=%ProgramFiles%\java\%%x
+REM set JAVA_HOME=%newestJDK%
 echo Using %JAVA_HOME%
 
 set ANT_OPTS=-Xms64m 
