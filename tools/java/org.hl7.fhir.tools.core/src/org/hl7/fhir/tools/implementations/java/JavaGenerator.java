2c556cdc704fcdd6e88d94c1818d4064bc7d4aee
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2016-07-03 06:09:30 +0000

    fix IG publisher dependencies
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@8649 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/implementations/java/JavaGenerator.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/implementations/java/JavaGenerator.java
@@ -794,6 +794,7 @@ public class JavaGenerator extends BaseGenerator implements PlatformGenerator {
     AddJarToJar(jar, Utilities.path(importsDir, "commons-lang3-3.3.2.jar"), names);
     AddJarToJar(jar, Utilities.path(importsDir, "commons-logging-1.1.1.jar"), names);
     AddJarToJar(jar, Utilities.path(importsDir, "commons-logging-api-1.1.jar"), names);
+    AddJarToJar(jar, Utilities.path(importsDir, "commons-lang3-3.3.2.jar"), names);
     AddJarToJar(jar, Utilities.path(importsDir, "httpclient-4.2.3.jar"), names);
     AddJarToJar(jar, Utilities.path(importsDir, "httpcore-4.2.2.jar"), names);
     AddJarToJar(jar, Utilities.path(importsDir, "hapi-fhir-base-1.3.jar"), names);
