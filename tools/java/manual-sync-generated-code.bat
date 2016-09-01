56abc7d0a15d897c657c8cf8ad973b20cf80ed0e
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2016-09-01 21:04:01 +0000

    Add Java Adornment template
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@9798 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ tools/java/manual-sync-generated-code.bat
@@ -0,0 +1,4 @@
+REM Dirmerge is a utility that scans source, and copies to dest. if the file already exists in source, it checks if they are different, and if they are, brings up winmerge if the genmark is not found
+
+..\..\tools\bin\dirmerge ..\..\implementations\java\org.hl7.fhir.dstu3\src\org\hl7\fhir\dstu3\model ..\..\tools\java\org.hl7.fhir.dstu3\src\org\hl7\fhir\dstu3\model java
+..\..\tools\bin\dirmerge ..\..\implementations\java\org.hl7.fhir.dstu3\src\org\hl7\fhir\dstu3\formats ..\..\tools\java\org.hl7.fhir.dstu3\src\org\hl7\fhir\dstu3\formats java
