ef6caa0c7c9f2799cc547cfd8ddb258fb9963d98
Chris Moesel <cmoesel@mitre.org>
2016-09-23 23:01:10 -0400

    Fix ServerURL in server.DefaultConfig
    The config's ServerURL is used by auth middleware to indicate redirects.  It must be a full URL -- so the default has been changed from "localhost" to "http://localhost:3001".

--- implementations/go/base/static/server/config.go
+++ implementations/go/base/static/server/config.go
@@ -11,7 +11,7 @@ var Database *mgo.Database
 
 // DefaultConfig is the default server configuration
 var DefaultConfig = Config{
-	ServerURL:       "localhost",
+	ServerURL:       "http://localhost:3001",
 	IndexConfigPath: "config/indexes.conf",
 	DatabaseName:    "fhir",
 	Auth:            auth.None(),
