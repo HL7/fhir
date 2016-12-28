a7249aa0522dd0304d4941fcdfee3649b88eccb6
emhaas <emhaas@2f0db536-2c49-4257-a3fa-e771ed206c19>
2016-12-28 23:10:32 +0000

    delete badslice
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@10639 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tests/badslice/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-name="ig10"
-path= /Users/ehaas/Documents/FHIR/build/tests/ig10/
-echo "================================================================="
-echo === Publish $name IG!!! $(date -u) ===
-echo "================================================================="
-git status
-java -jar /Users/ehaas/Downloads/org.hl7.fhir.igpublisher.jar -ig "${path}ig.json"
