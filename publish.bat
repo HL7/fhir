988b697ae4351c15bc2186e9c128517444e70516
oliverkrauss <oliverkrauss@2f0db536-2c49-4257-a3fa-e771ed206c19>
2017-01-14 15:14:57 +0000

    #12573 Added Worfklow Mappings for Activitydefinition, Careplan, Communication, CommunicationRequest, OperationDefinition, PlanDefinition
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@10755 2f0db536-2c49-4257-a3fa-e771ed206c19

--- publish.bat
+++ publish.bat
@@ -1,8 +1,7 @@
 @ECHO off
 ECHO Running publication process now
 
-for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newestJDK=%ProgramFiles%\java\%%x
-set JAVA_HOME=%newestJDK%
+
 echo Using %JAVA_HOME%
 
 set ANT_OPTS=-Xms96m 
