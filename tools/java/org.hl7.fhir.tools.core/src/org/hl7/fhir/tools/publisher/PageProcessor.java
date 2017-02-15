66df9ac2b35cf1d3c9304e3353f256fef913e888
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2017-02-15 23:35:36 +0000

    fix broken link for Melva
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@11201 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
@@ -3138,9 +3138,9 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider, IReferen
     StringBuilder b = new StringBuilder();
     b.append("<ul class=\"nav nav-tabs\">");
     b.append(makeHeaderTab("Dosage Instruction Detail", "dosageinstruction.html", mode==null || "base".equals(mode)));
-    b.append(makeHeaderTab("Examples", "dosageinstruction-examples.html", mode==null || "examples".equals(mode)));
-    b.append(makeHeaderTab("Detailed Descriptions", "dosageinstruction-definitions.html", mode==null || "definitions".equals(mode)));
-    b.append(makeHeaderTab("Mappings", "dosageinstruction-mappings.html", mode==null || "mappings".equals(mode)));
+    b.append(makeHeaderTab("Examples", "dosage-examples.html", mode==null || "examples".equals(mode)));
+    b.append(makeHeaderTab("Detailed Descriptions", "dosage-definitions.html", mode==null || "definitions".equals(mode)));
+    b.append(makeHeaderTab("Mappings", "dosage-mappings.html", mode==null || "mappings".equals(mode)));
     b.append("</ul>\r\n");
     return b.toString();
   }
