0e8f493d3a72a973ddb00a5579e6a0bd3bfa98e2
Carlton Duffett <cjduffett@comcast.net>
2017-02-16 15:05:13 -0500

    Narrowed search for _id

--- implementations/go/base/static/server/resource_controller.go
+++ implementations/go/base/static/server/resource_controller.go
@@ -292,7 +292,7 @@ func (u CustomJSONRenderer) Render(w http.ResponseWriter) (err error) {
 	data = bytes.Replace(data, []byte("\\u0026"), []byte("&"), -1)
 
 	// Convert "_id" to "id"
-	data = bytes.Replace(data, []byte("_id"), []byte("id"), -1)
+	data = bytes.Replace(data, []byte("\"_id\":"), []byte("\"id\":"), -1)
 
 	writeContentType(w, fhirJSONContentType)
 	_, err = w.Write(data)
