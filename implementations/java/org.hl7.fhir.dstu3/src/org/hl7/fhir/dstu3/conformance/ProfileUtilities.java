eb6d67ef879e3648f2bcc0e2e5cd5f23bdb1af9a
lloyd_mckenzie <lloyd_mckenzie@2f0db536-2c49-4257-a3fa-e771ed206c19>
2017-03-12 15:06:20 +0000

    Comment out debug lines
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@11589 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.dstu3/src/org/hl7/fhir/dstu3/conformance/ProfileUtilities.java
+++ implementations/java/org.hl7.fhir.dstu3/src/org/hl7/fhir/dstu3/conformance/ProfileUtilities.java
@@ -352,8 +352,8 @@ public class ProfileUtilities extends TranslatingUtilities {
     }
     for (ElementDefinition e : derived.getDifferential().getElement()) {
       if (!snapshotElements.containsKey(e.getId()))
-//        throw new DefinitionException("Snapshot does not contain differential element with id: " + e.getId());
-        System.out.println("**BAD Differential element: " + profileName + ":" + e.getId());
+        throw new DefinitionException("Snapshot does not contain differential element with id: " + e.getId());
+//        System.out.println("**BAD Differential element: " + profileName + ":" + e.getId());
     }
   }
 
@@ -365,13 +365,13 @@ public class ProfileUtilities extends TranslatingUtilities {
   private void processPaths(String indent, StructureDefinitionSnapshotComponent result, StructureDefinitionSnapshotComponent base, StructureDefinitionDifferentialComponent differential, int baseCursor, int diffCursor, int baseLimit,
       int diffLimit, String url, String profileName, String contextPathSrc, String contextPathDst, boolean trimDifferential, String contextName, String resultPathBase, boolean slicingDone) throws DefinitionException, FHIRException {
 
-    System.out.println(indent+"PP @ "+resultPathBase+": base = "+baseCursor+" to "+baseLimit+", diff = "+diffCursor+" to "+diffLimit+" (slicing = "+slicingDone+")");
+//    System.out.println(indent+"PP @ "+resultPathBase+": base = "+baseCursor+" to "+baseLimit+", diff = "+diffCursor+" to "+diffLimit+" (slicing = "+slicingDone+")");
     // just repeat processing entries until we run out of our allowed scope (1st entry, the allowed scope is all the entries)
     while (baseCursor <= baseLimit) {
       // get the current focus of the base, and decide what to do
       ElementDefinition currentBase = base.getElement().get(baseCursor);
       String cpath = fixedPath(contextPathSrc, currentBase.getPath());
-      System.out.println(indent+" - "+cpath+": base = "+baseCursor+" to "+baseLimit+", diff = "+diffCursor+" to "+diffLimit+" (slicing = "+slicingDone+")");
+//      System.out.println(indent+" - "+cpath+": base = "+baseCursor+" to "+baseLimit+", diff = "+diffCursor+" to "+diffLimit+" (slicing = "+slicingDone+")");
       List<ElementDefinition> diffMatches = getDiffMatches(differential, cpath, diffCursor, diffLimit, profileName, url); // get a list of matching elements in scope
 
       // in the simple case, source is not sliced.
