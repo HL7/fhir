98d1fa165a04c9ae884d90873f7ad6c9e20ca3af
davidpyke <davidpyke@2f0db536-2c49-4257-a3fa-e771ed206c19>
2017-04-28 20:05:38 +0000

    linux people need cleaning too
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@11967 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ clean.sh
@@ -0,0 +1,17 @@
+find -name *bak -delete
+find -name *class -delete
+cd temp
+find . -type f -delete
+cd ..
+cd guides/ccda2/output
+find . -type f -delete
+cd ../../..
+cd guides/ccda2/qa
+find . -type f -delete
+cd ../../..
+cd guides/ccda2/temp
+find . -type f -delete
+cd ../../..
+cd publish
+find . -type f -delete
+cd ..
