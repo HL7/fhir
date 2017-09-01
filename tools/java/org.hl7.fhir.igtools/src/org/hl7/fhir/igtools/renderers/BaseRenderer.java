535be7fdb8c106e8023f90c9a00e4d4e1f26ce61
lloyd_mckenzie <lloyd_mckenzie@2f0db536-2c49-4257-a3fa-e771ed206c19>
2017-09-01 05:24:04 +0000

    Allow markdown hyperlinks to pages within the IG instead of only to the FHIR spec.
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@12340 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.igtools/src/org/hl7/fhir/igtools/renderers/BaseRenderer.java
+++ tools/java/org.hl7.fhir.igtools/src/org/hl7/fhir/igtools/renderers/BaseRenderer.java
@@ -73,7 +73,7 @@ public class BaseRenderer extends TranslatingUtilities {
 	      while (i > 0) {
 	        if (text.substring(i, i+2).equals("](") && i+7 <= text.length()) {
 	          // The following can go horribly wrong if i+7 > text.length(), thus the check on i+7 above and the Throwable catch around the whole method just in case. 
-	          if (!text.substring(i, i+7).equals("](http:") && !text.substring(i, i+8).equals("](https:")) { 
+	          if (!text.substring(i, i+7).equals("](http:") && !text.substring(i, i+8).equals("](https:") && !text.substring(i, i+3).equals("](.")) { 
 	            text = text.substring(0, i)+"]("+prefix+text.substring(i+2);
 	          }
 	        }
