e0e4c78bef6c23db38b00cd24ff7b11fc9a30349
lloyd_mckenzie <lloyd_mckenzie@2f0db536-2c49-4257-a3fa-e771ed206c19>
2017-09-28 05:27:11 +0000

    Fixed issue with type discriminators not working properly for [x] elements
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@12387 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.r4/src/org/hl7/fhir/r4/utils/FHIRPathEngine.java
+++ implementations/java/org.hl7.fhir.r4/src/org/hl7/fhir/r4/utils/FHIRPathEngine.java
@@ -2907,10 +2907,8 @@ public class FHIRPathEngine {
           throw new DefinitionException("Problem with use of resolve() - profile '"+element.getType().get(0).getProfile()+"' on "+element.getId()+" could not be resolved");
         childDefinitions = ProfileUtilities.getChildMap(sd, sd.getSnapshot().getElementFirstRep());
       }
-      // Lloyd: When parsing, foo[x] gets turned into foo.[x] which doesn't work with tail matches.  Not sure if this is intended or not, but adding this as a work-around for now
-      String match = expr.toString().endsWith(".[x]") ? expr.toString().substring(0,expr.toString().length()-4) + "[x]" : expr.getName();
       for (ElementDefinition t : childDefinitions) {
-        if (tailMatches(t, match)) {
+        if (tailMatches(t, expr.getName())) {
           focus = t;
           break;
         }
@@ -2954,11 +2952,6 @@ public class FHIRPathEngine {
       throw new DefinitionException("Unable to resolve discriminator");      
     else if (expr.getInner() == null)
       return focus;
-    else if (expr.getInner().parameterCount() ==1 && expr.getInner().getParameters().get(0).getName().equals("x"))
-      if (expr.getInner().getInner() == null)
-        return focus;
-      else
-        return evaluateDefinition(expr.getInner().getInner(), sd, focus);
     else
       return evaluateDefinition(expr.getInner(), sd, focus);
   }
@@ -2981,8 +2974,12 @@ public class FHIRPathEngine {
     String tail = tailDot(t.getPath());
     if (d.contains("["))
       return tail.startsWith(d.substring(0, d.indexOf('[')));
-    else
-      return tail.equals(d);
+    else if (tail.equals(d))
+      return true;
+    else if (t.getType().size()==1 && t.getPath().toUpperCase().endsWith(t.getType().get(0).getCode().toUpperCase()))
+      return tail.startsWith(d);
+    
+    return false;
   }
 
   private String tailDot(String path) {
