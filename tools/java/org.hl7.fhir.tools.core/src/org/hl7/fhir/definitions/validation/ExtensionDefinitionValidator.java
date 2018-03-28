5649042bc4dcba41ba1a6de8dc4c3cbbef375a3a
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2018-03-28 20:51:33 +0000

    Add language extension
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@13554 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/definitions/validation/ExtensionDefinitionValidator.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/definitions/validation/ExtensionDefinitionValidator.java
@@ -29,7 +29,7 @@ public class ExtensionDefinitionValidator {
       if (ed.getPath().startsWith("Extension.value") && !"0".equals(ed.getMax())) {
         for (TypeRefComponent tr : ed.getType()) {
           if ("code".equals(tr.getCode())) {
-            if (!ed.hasBinding() || ed.getBinding().getStrength() != BindingStrength.REQUIRED)
+            if (!ed.hasBinding() || (ed.getBinding().getStrength() != BindingStrength.REQUIRED && !ed.getBinding().hasExtension("http://hl7.org/fhir/StructureDefinition/elementdefinition-maxValueSet")))
               throw new FHIRException("Extension "+sd.getUrl()+" has an element of type 'code' which must have required binding");
           }
         }
