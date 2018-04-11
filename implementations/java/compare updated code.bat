b252d2021112b57c2af991af8b338c3c6ca15c48
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2018-04-11 05:47:59 +0000

    fix build
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@13693 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/compare updated code.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\WinMerge\WinMergeU.exe" ..\..\temp\java\org.hl7.fhir.r4\src\org\hl7\fhir\r4\model\ org.hl7.fhir.r4\src\org\hl7\fhir\r4\model\
+"C:\Program Files (x86)\WinMerge\WinMergeU.exe" ..\..\temp\java\org.hl7.fhir.r4\src\org\hl7\fhir\r4\formats\ org.hl7.fhir.r4\src\org\hl7\fhir\r4\formats\
