2a547298798940cfeaa4ab1b25c524ea50ba200f
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2018-04-15 06:09:57 +0000

    fix build
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@13706 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/org.hl7.fhir.convertors/src/org/hl7/fhir/convertors/IGR2ConvertorAdvisor.java
@@ -0,0 +1,41 @@
+package org.hl7.fhir.convertors;
+
+import org.hl7.fhir.dstu2.model.Resource;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.ValueSet;
+import org.hl7.fhir.exceptions.FHIRException;
+
+public class IGR2ConvertorAdvisor implements VersionConvertorAdvisor40 {
+
+  @Override
+  public boolean ignoreEntry(BundleEntryComponent src) {
+    return false;
+  }
+
+  @Override
+  public Resource convertR2(org.hl7.fhir.r4.model.Resource resource) throws FHIRException {
+    return null;
+  }
+
+  @Override
+  public org.hl7.fhir.dstu2016may.model.Resource convertR2016May(org.hl7.fhir.r4.model.Resource resource) throws FHIRException {
+    return null;
+  }
+
+  @Override
+  public org.hl7.fhir.dstu3.model.Resource convertR3(org.hl7.fhir.r4.model.Resource resource) throws FHIRException {
+    return null;
+  }
+
+  @Override
+  public void handleCodeSystem(CodeSystem cs, ValueSet vs) {
+    cs.setId(vs.getId());
+  }
+
+  @Override
+  public CodeSystem getCodeSystem(ValueSet src) {
+    return null;
+  }
+
+}
