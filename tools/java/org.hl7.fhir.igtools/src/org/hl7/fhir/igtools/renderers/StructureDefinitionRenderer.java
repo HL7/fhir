6ba5c746dd4e4d82c4d6348b394349d33b2a1278
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2018-06-01 01:26:17 +0000

    add self link in dictionaries in IGs
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@13814 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.igtools/src/org/hl7/fhir/igtools/renderers/StructureDefinitionRenderer.java
+++ tools/java/org.hl7.fhir.igtools/src/org/hl7/fhir/igtools/renderers/StructureDefinitionRenderer.java
@@ -480,12 +480,12 @@ public class StructureDefinitionRenderer extends BaseRenderer {
           StructureDefinition extDefn = context.fetchResource(StructureDefinition.class, ec.getType().get(0).getProfile().get(0).getValue());
           if (extDefn == null) {
             String title = ec.getPath() + " ("+(ec.getType().get(0).getProfile().get(0).getValue().startsWith("#") ? sd.getUrl() : "")+ec.getType().get(0).getProfile()+")";
-            b.append("  <tr><td colspan=\"2\" class=\"structure\"><a name=\""+ec.getId()+"\"> </a><b>"+title+"</b></td></tr>\r\n");
+            b.append("  <tr><td colspan=\"2\" class=\"structure\"><span class=\"self-link-parent\"><a name=\""+ec.getId()+"\"> </a><b>"+title+"</b>"+link(ec.getId())+"</span></td></tr>\r\n");
             generateElementInner(b, sd,  ec, 1, null);
           } else {
             String title = ec.getPath() + " (<a href=\""+(extDefn.hasUserData("path") ? extDefn.getUserData("path") : "extension-"+extDefn.getId().toLowerCase()+".html")+
                 "\">"+(ec.getType().get(0).getProfile().get(0).getValue().startsWith("#") ? sd.getUrl() : "")+ec.getType().get(0).getProfile()+"</a>)";
-            b.append("  <tr><td colspan=\"2\" class=\"structure\"><a name=\""+ec.getId()+"\"> </a>");
+            b.append("  <tr><td colspan=\"2\" class=\"structure\"><span class=\"self-link-parent\"><a name=\""+ec.getId()+"\"> </a>");
             if (ec.getId().endsWith("[x]")) {
               Set<String> tl = new HashSet<String>();
               for (TypeRefComponent tr : ec.getType()) {
@@ -498,13 +498,13 @@ public class StructureDefinitionRenderer extends BaseRenderer {
                 }
               }
             }
-            b.append("<b>"+title+"</b></td></tr>\r\n");
+            b.append("<b>"+title+"</b>"+link(ec.getId())+"</span></td></tr>\r\n");
             ElementDefinition valueDefn = getExtensionValueDefinition(extDefn);
             generateElementInner(b, extDefn, extDefn.getSnapshot().getElement().get(0), valueDefn == null ? 2 : 3, valueDefn);
           }
         } else {
           String title = ec.getPath() + (!ec.hasSliceName() ? "" : "(" +ec.getSliceName() +")");
-          b.append("  <tr><td colspan=\"2\" class=\"structure\"><a name=\""+ec.getId()+"\"> </a>");
+          b.append("  <tr><td colspan=\"2\" class=\"structure\"><span class=\"self-link-parent\"><a name=\""+ec.getId()+"\"> </a>");
           if (ec.getId().endsWith("[x]")) {
             Set<String> tl = new HashSet<String>();
             for (TypeRefComponent tr : ec.getType()) {
@@ -526,7 +526,7 @@ public class StructureDefinitionRenderer extends BaseRenderer {
           for (StringPair s : replacements)
             if (ec.getId().startsWith(s.match))
               b.append("<a name=\""+s.replace+ec.getId().substring(s.match.length())+"\"> </a>");
-          b.append("<b>"+title+"</b></td></tr>\r\n");
+          b.append("<b>"+title+"</b>"+link(ec.getId())+"</span></td></tr>\r\n");
           generateElementInner(b, sd, ec, 1, null);
           if (ec.hasSlicing())
             generateSlicing(sd, ec.getSlicing());
@@ -536,6 +536,10 @@ public class StructureDefinitionRenderer extends BaseRenderer {
     b.append("</table>\r\n");
     i++;
     return b.toString();
+  }
+
+  private String link(String id) {
+    return "<a href=\"#"+id+"\" title=\"link to here\" class=\"self-link\"><svg viewBox=\"0 0 1792 1792\" width=\"16\" class=\"self-link\" height=\"16\"><path d=\"M1520 1216q0-40-28-68l-208-208q-28-28-68-28-42 0-72 32 3 3 19 18.5t21.5 21.5 15 19 13 25.5 3.5 27.5q0 40-28 68t-68 28q-15 0-27.5-3.5t-25.5-13-19-15-21.5-21.5-18.5-19q-33 31-33 73 0 40 28 68l206 207q27 27 68 27 40 0 68-26l147-146q28-28 28-67zm-703-705q0-40-28-68l-206-207q-28-28-68-28-39 0-68 27l-147 146q-28 28-28 67 0 40 28 68l208 208q27 27 68 27 42 0 72-31-3-3-19-18.5t-21.5-21.5-15-19-13-25.5-3.5-27.5q0-40 28-68t68-28q15 0 27.5 3.5t25.5 13 19 15 21.5 21.5 18.5 19q33-31 33-73zm895 705q0 120-85 203l-147 146q-83 83-203 83-121 0-204-85l-206-207q-83-83-83-203 0-123 88-209l-88-88q-86 88-208 88-120 0-204-84l-208-208q-84-84-84-204t85-203l147-146q83-83 203-83 121 0 204 85l206 207q83 83 83 203 0 123-88 209l88 88q86-88 208-88 120 0 204 84l208 208q84 84 84 204z\" fill=\"navy\"></path></svg></a>";
   }
 
   private boolean isProfiledExtension(ElementDefinition ec) {
