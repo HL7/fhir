05da643f547aa6dcd70a7228367ef872180b69c8
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2018-07-02 23:23:46 +0000

    fix clean file
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@13890 2f0db536-2c49-4257-a3fa-e771ed206c19

--- clean.bat
+++ clean.bat
@@ -1,18 +1,8 @@
 del *.bak /s /Q
 del *.class /s /Q
-cd temp
-del *.* /s /Q
-cd ..
-cd guides/ccda2/output
-del *.* /s /Q
-cd ../../..
-cd guides/ccda2/qa
-del *.* /s /Q
-cd ../../..
-cd guides/ccda2/temp
-del *.* /s /Q
-cd ../../..
-cd publish
-del *.* /s /Q
-cd ..
+del temp/*.* /s /Q
+del guides/ccda2/output/*.* /s /Q
+del guides/ccda2/qa/*.* /s /Q
+del guides/ccda2/temp/*.* /s /Q
+del publish/*.* /s /Q
 
