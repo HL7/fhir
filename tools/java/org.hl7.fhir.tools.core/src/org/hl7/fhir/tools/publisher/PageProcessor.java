22d5233b31f33ae541736868584dea6fc8790a50
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2018-08-02 00:05:58 +0000

    GF#16877 - fix wrong links
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@14308 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
@@ -676,6 +676,8 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider, IReferen
         src = s1+edHeader(com.length > 1 ? com[1] : null)+s3;
       else if (com[0].equals("elheader"))
         src = s1+elHeader(com.length > 1 ? com[1] : null)+s3;
+      else if (com[0].equals("belheader"))
+        src = s1+belHeader(com.length > 1 ? com[1] : null)+s3;
       else if (com[0].equals("extheader"))
         src = s1+extHeader(com.length > 1 ? com[1] : null)+s3;
       else if (com[0].equals("mmheader"))
@@ -3964,6 +3966,15 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider, IReferen
     return b.toString();
   }
 
+  private String belHeader(String mode) {
+    StringBuilder b = new StringBuilder();
+    b.append("<ul class=\"nav nav-tabs\">");
+    b.append(makeHeaderTab("Element", "backboneelement.html", mode==null || "base".equals(mode)));
+    b.append(makeHeaderTab("Detailed Descriptions", "backboneelement-definitions.html", mode==null || "definitions".equals(mode)));
+    b.append("</ul>\r\n");
+    return b.toString();
+  }
+
   private String extHeader(String mode) {
     StringBuilder b = new StringBuilder();
     b.append("<ul class=\"nav nav-tabs\">");
@@ -4808,7 +4819,7 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider, IReferen
       else if (com.length == 2 && com[0].equals("dictionary"))
         src = s1+dictForDt(com[1])+s3;
       else if (com[0].equals("pageheader") || com[0].equals("dtheader") || com[0].equals("mdtheader") || com[0].equals("edheader") || com[0].equals("mmheader") ||
-          com[0].equals("drheader") || com[0].equals("elheader") || com[0].equals("extheader") || com[0].equals("narrheader") ||
+          com[0].equals("drheader") || com[0].equals("elheader") || com[0].equals("belheader") || com[0].equals("extheader") || com[0].equals("narrheader") ||
           com[0].equals("formatsheader") || com[0].equals("resourcesheader") || com[0].equals("txheader") || com[1].equals("txheader0") ||
           com[0].equals("refheader") || com[0].equals("extrasheader") || com[0].equals("profilesheader") || com[0].equals("fmtheader") ||
           com[0].equals("igheader") || com[0].equals("cmpheader") || com[0].equals("atomheader") || com[0].equals("dictheader") ||
@@ -5216,7 +5227,7 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider, IReferen
       else if (com.length == 2 && com[0].equals("dictionary"))
         src = s1+dictForDt(com[1])+s3;
       else if (com[0].equals("pageheader") || com[0].equals("dtheader") || com[0].equals("mdtheader") || com[0].equals("edheader") || com[0].equals("mmheader") ||
-          com[0].equals("drheader") ||com[0].equals("elheader") || com[0].equals("extheader") || com[0].equals("resourcesheader") ||
+          com[0].equals("drheader") ||com[0].equals("elheader") || com[0].equals("belheader") || com[0].equals("extheader") || com[0].equals("resourcesheader") ||
           com[0].equals("formatsheader") || com[0].equals("narrheader") || com[0].equals("refheader") ||  com[0].equals("extrasheader") || com[0].equals("profilesheader") ||
           com[0].equals("txheader") || com[0].equals("txheader0") || com[0].equals("fmtheader") || com[0].equals("igheader") ||
           com[0].equals("cmpheader") || com[0].equals("atomheader") || com[0].equals("dictheader") || com[0].equals("ctheader") ||
