2047612ce2d18103728391de1d6e965cf8b0b48a
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2018-08-17 23:57:47 +0000

    support all loinc vs
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk/build@14895 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/java/org.hl7.fhir.r4/src/org/hl7/fhir/r4/terminologies/ImplicitValueSets.java
+++ implementations/java/org.hl7.fhir.r4/src/org/hl7/fhir/r4/terminologies/ImplicitValueSets.java
@@ -12,7 +12,7 @@ public class ImplicitValueSets {
     
     if (url.startsWith("http://snomed.info/sct"))
       return buildSnomedValueSet(url);
-    if (url.startsWith("http://loinc.org/vs/"))
+    if (url.startsWith("http://loinc.org/vs"))
       return buildLoincValueSet(url);
     if (url.equals("http://unitsofmeasure.org/vs"))
       return allUcumValueSet();
@@ -32,6 +32,13 @@ public class ImplicitValueSets {
       vs.setTitle("Loinc Implicit ValueSet for "+url.substring(21));
       // todo: populate the compose fro the terminology server
       return vs;   
+    } else if (url.equals("http://loinc.org/vs")) {
+      ValueSet vs = new ValueSet();
+      vs.setUrl(url);
+      vs.setStatus(PublicationStatus.ACTIVE);
+      vs.setName("LoincVSAll");
+      vs.setTitle("Loinc Implicit ValueSet : all codes");
+      // todo: populate the compose for the terminology server
     } else {
       return null;
     }
