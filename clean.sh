08a7e21d410772a8ac1d294c487c3fb3e60bc418
Harold Solbrig <solbrig@jhu.edu>
2019-02-20 15:58:58 -0600

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,9 @@
-find -name *bak -delete
-find -name *class -delete
-find temp/. -type f -delete
-find publish/. -type f -delete
+read -p "Remove all unnecessary files in $(pwd)? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   find -name *\.bak -delete
+   find -name *\.class -delete
+   find temp/.  -type f -delete
+   find publish/. -type f  -delete
+fi
