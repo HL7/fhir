34b3e0c8bbfa37d5732d9c5fcc8518d980f4adb9
Grahame Grieve <grahameg@gmail.com>
2019-03-25 07:24:19 +1100

    for fixing presentation of checks

--- tools/java/org.hl7.fhir.igtools/src/org/hl7/fhir/igtools/publisher/BallotChecker.java
+++ tools/java/org.hl7.fhir.igtools/src/org/hl7/fhir/igtools/publisher/BallotChecker.java
@@ -20,30 +20,37 @@ public class BallotChecker {
     this.folder = folder;
   }
 
-  public String check(String canonical, String packageId, String version, String historyPage) throws IOException {  
+  public String check(String canonical, String packageId, String version, String historyPage) throws IOException { 
+    if (!canonical.startsWith("http://hl7.org") && !canonical.startsWith("http://fhir.org"))
+      return "<ul><li>n/a - not an HL7.org or FHIR.org implementation guide</li></ul>\r\n";
+    
+    List<String> errors = new ArrayList<String>();
+    if (!Utilities.existsInList(historyPage, Utilities.path(canonical, "history.html"), Utilities.path(canonical, "history.cfml")))
+      errors.add("History Page '"+historyPage+"' is wrong (ig.json#paths/history) - must be '"+Utilities.path(canonical, "history.html")+"' or '"+Utilities.path(canonical, "history.cfml")+"'");
+    
+    JsonObject json = null;
     String plfn = Utilities.path(folder, "package-list.json");
     File f = new File(plfn);
     if (!f.exists())
-      return "No package-list.json file found in "+folder;
+      errors.add("package-list.json: file not found in "+folder);
     else {
-      JsonObject json = null;
       try {
         json = (JsonObject) new com.google.gson.JsonParser().parse(TextFile.fileToString(f));
       } catch (Exception e) {
-        return "Error parsing Package List: " +e.getMessage();
+        errors.add("package-list.json: " +e.getMessage());
       }
-      List<String> errors = new ArrayList<String>();
-      if (!Utilities.existsInList(historyPage, Utilities.path(canonical, "history.html"), Utilities.path(canonical, "history.cfml")))
-        errors.add("History Page '"+historyPage+"' is wrong (ig.json#paths/history) - must be '"+Utilities.path(canonical, "history.html")+"' or '"+Utilities.path(canonical, "history.cfml")+"'");
+    }
+    
+    if (json != null) {
       if (!json.has("package-id"))
-        errors.add("No Package Id");
+        errors.add("package-list.json: No Package Id");
       else if (!json.get("package-id").getAsString().equals(packageId))
-        errors.add("package-id is wrong - is '"+json.get("package-id").getAsString()+"' should be '"+packageId+"'");
+        errors.add("package-list.json: package-id is wrong - is '"+json.get("package-id").getAsString()+"' should be '"+packageId+"'");
       
       if (!json.has("canonical"))
-        errors.add("No Canonical URL");
+        errors.add("package-list.json: No Canonical URL");
       else if (!json.get("canonical").getAsString().equals(canonical))
-        errors.add("canonical is wrong - is '"+json.get("canonical").getAsString()+"' should be '"+canonical+"'");
+        errors.add("package-list.json: canonical is wrong - is '"+json.get("canonical").getAsString()+"' should be '"+canonical+"'");
 
       JsonArray list = json.getAsJsonArray("list");
       boolean found = false;
@@ -52,28 +59,32 @@ public class BallotChecker {
         if (o.has("version") && o.get("version").getAsString().equals(version)) {
           found = true;
           if (!o.has("desc") && !o.has("changes"))
-            errors.add("package-list entry must have a 'desc' or 'changes' (or both)");
+            errors.add("package-list.json entry: must have a 'desc' or 'changes' (or both)");
           if (!o.has("date"))
-            errors.add("package-list entry must have a 'desc' (though the value doesn't matter)");
+            errors.add("package-list.json entry: must have a 'desc' (though the value doesn't matter)");
           if (!o.has("status"))
-            errors.add("package-list entry must have a 'status' that describes the ballot status");
+            errors.add("package-list.json entry: must have a 'status' that describes the ballot status");
           if (!o.has("sequence"))
-            errors.add("package-list entry must have a 'sequence' that describes the ballot goal");
+            errors.add("package-list.json entry: must have a 'sequence' that describes the ballot goal");
           if (!o.has("path"))
-            errors.add("package-list entry must have a 'path' where it will be published");
+            errors.add("package-list.json entry: must have a 'path' where it will be published");
           else if (!o.get("path").getAsString().startsWith(canonical))
-            errors.add("package-list entry must have a 'path' that starts with the canonical (is '"+o.get("path").getAsString()+"', should start with '"+canonical+"'");
-          if (errors.size() > 0)
-            return "Errors: "+errors.toString();              
-          else
-            return "All ok (path - "+o.get("path").getAsString();              
+            errors.add("package-list.json entry: must have a 'path' that starts with the canonical (is '"+o.get("path").getAsString()+"', should start with '"+canonical+"'");
+          if (errors.size() == 0)
+            errors.add("All ok (path - "+o.get("path").getAsString());              
             
         }
       }
       if (!found)
-        errors.add("No entry found for version "+version);     
-      return "Errors: "+errors.toString();              
+        errors.add("package-list.json: No entry found for version "+version);
     }
+    StringBuilder b = new StringBuilder();
+    b.append("<ul>\r\n");
+    for (String s : errors) {
+      b.append("  <li>"+Utilities.escapeXml(s)+"</li>\r\n");
+    }
+    b.append("</ul>\r\n");
+    return b.toString();              
   }
 
 }
