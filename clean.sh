a962a567b2cc6083f5a65d48e217024062de6737
hsolbrig <solbrig@jhu.edu>
2019-09-14 16:33:00 -0400

    Move Statistic and OrderedDistribution off of the datatypes page.
    This won't actually compile until we can get the java in the temp directory into the
    build process

--- clean.sh
+++ clean.sh
@@ -2,8 +2,8 @@ read -p "Remove all unnecessary files in $(pwd)? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-   find -name *\.bak -delete
-   find -name *\.class -delete
+   find . -name *\.bak -delete
+   find . -name *\.class -delete
    find temp/.  -type f -delete
    find publish/. -type f  -delete
 fi
