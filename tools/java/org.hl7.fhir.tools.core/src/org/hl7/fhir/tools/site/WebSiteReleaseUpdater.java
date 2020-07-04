641f101e6645af7ec8d583c5bb9a23a44723d8c3
Vadim Peretokin <vperetokin@gmail.com>
2020-07-04 12:24:49 +0200

    Typo fix

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/site/WebSiteReleaseUpdater.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/site/WebSiteReleaseUpdater.java
@@ -58,7 +58,7 @@ public class WebSiteReleaseUpdater {
     
     if (lastInSequence) {
       if (ver == currentVer)
-        return "<p id=\"publish-box-milestone\">This page is part of FHIR "+sequence+" - the current version v"+version+" - in it's permanent home (it will always be available at this URL). "+
+        return "<p id=\"publish-box-milestone\">This page is part of FHIR "+sequence+" - the current version v"+version+" - in its permanent home (it will always be available at this URL). "+
              "For a full list of available versions, see the "+
              "<a href=\"http://hl7.org/fhir/directory.html\">Directory of published versions <img style=\"vertical-align: baseline\" src=\"{pd}external.png\"></a>.</p>";
       else
