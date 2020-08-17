d54279874d27ecff301608523e6e5c5146fda7ad
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2020-08-17 14:34:59 +0200

    remove empty header

--- publish.bat
+++ publish.bat
@@ -1,6 +1,8 @@
 @ECHO off
 ECHO Running publication process now
 
+SET PATH=%~dp0apache-ant\bin;%PATH%;
+
 for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newestJDK=%ProgramFiles%\java\%%x
 set JAVA_HOME=%newestJDK%
 echo Using %JAVA_HOME%
