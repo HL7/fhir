0c36c1f95601f2437b38f8195b893245dffd32c4
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2021-02-04 16:29:42 -0700

    Added args processing to publish.bat

--- publish.bat
+++ publish.bat
@@ -3,5 +3,17 @@ ECHO Running publication process now
 for /f "usebackq" %%x in (`dir /od /b "%ProgramFiles%\java\jdk*"`) do set newestJDK=%ProgramFiles%\java\%%x
 set JAVA_HOME=%newestJDK%
 echo Using %JAVA_HOME%
+
+if "%~1"=="" goto blank
+
 call ./gradlew publish --args="%*"
+
+goto done
+
+:blank
+
+call ./gradlew publish
+
+:done
+
 PAUSE
